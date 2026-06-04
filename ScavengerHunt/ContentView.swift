ed5b7aefcb1214d233b919a1f05b1220dfd0fa4c
Sheikh Naim <naimbgd@gmail.com>
2026-06-04 09:14:20 -0400

    Initial Commit

---
+++ ScavengerHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScavengerHunt
+//
+//  Created by Sheikh Naim on 2026-06-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScavengerHunt/ScavengerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntApp.swift
+//  ScavengerHunt
+//
+//  Created by Sheikh Naim on 2026-06-04.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
